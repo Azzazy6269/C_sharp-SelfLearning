0dac7028761ac8ea163f63ab112ae33664966d0b
Azzazy6269 <mohamadelazzazy@gmail.com>
2025-09-04 01:03:27 +0300

    Upload StringExtensionMethod task

---
+++ Tutorial 22 - Extension methods/3- StringExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Console.ReadLine().WordCount());
+
+    }
+}
+
+static class StringExtension
+{
+    public static int WordCount(this String value)
+    {
+        string [] newValue =  value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return newValue.Length;
+    }
+}
